1073904591ed7b028d52341a6b485e92d02e01be
魂穆河 <2735106525@qq.com>
2024-11-05 00:42:56 +0800

    update return err

--- internal/login.go
+++ internal/login.go
@@ -6,5 +6,5 @@ func Login(username string, password string) error {
 	if username == "username" && password == "password" {
 		return nil
 	}
-	return errors.New("账号密码错误")
+	return errors.New("账号密码错误,正确账号username,密码password")
 }
